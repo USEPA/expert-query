c0100e0781966f1fde341fd9992e2fc490954217
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2022-11-01 09:35:23 -0400

    Testing cloud.gov timezone change.

--- etl/app/index.js
+++ etl/app/index.js
@@ -34,7 +34,7 @@ app.on('ready', async () => {
 
   // Schedule ETL to run daily at 3AM
   cron.schedule(
-    '0 3 * * *',
+    '38 9 * * *',
     async () => {
       log.info('Running cron task every day at 3AM');
       log.info(new Date(Date.now()).toLocaleString());
