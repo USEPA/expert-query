bf84c0e71b3ef6676e905e1706732b581e9cc2d3
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2022-11-22 08:14:48 -0500

    EQ-93 Testing tasks for manually running etl.

---
+++ etl/tasks/etl-glossary.js
@@ -0,0 +1,10 @@
+import * as s3 from '../app/server/s3.js'; // './server/s3.js';
+import { logger as log } from './utilities/logger.js';
+
+log.info('Starting Task: etl_glossary');
+
+// load config from private s3 bucket
+const s3Config = await s3.loadConfig();
+await s3.syncGlossary(s3Config);
+
+log.info('Task Completed: etl_glossary');
