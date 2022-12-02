0ccd62c397eb601b6192417da190c997664ab120
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2022-12-02 14:47:00 -0500

    EQ-99 Added the assessment units with monitoring locations profile to the etl.

--- etl/app/server/profiles/index.js
+++ etl/app/server/profiles/index.js
@@ -1,2 +1,3 @@
 export * as assessmentUnits from './assessmentUnits.js';
+export * as assessmentUnitsMonitoringLocations from './assessmentUnitsMonitoringLocations.js';
 export * as gisAssessments from './gisAssessments.js';
