4a249edd24f85758b1c7048cd6113e0fdf31a166
Max Diebold <max.diebold@erg.com>
2022-12-15 18:28:37 -0500

    Fixed a code smell

--- app/client/src/components/sourceSelect.tsx
+++ app/client/src/components/sourceSelect.tsx
@@ -144,7 +144,7 @@ export default function SourceSelect({
       onBlur={(ev) => {
         if (
           !ev.currentTarget.contains(ev.relatedTarget) ||
-          !(ev.relatedTarget?.tagName === 'LI')
+          ev.relatedTarget?.tagName !== 'LI'
         ) {
           setSourcesVisible(false);
         }
