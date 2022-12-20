ce2d0207a41c2e8c8e0d0d6a45423fc4cd9ed032
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2022-12-20 13:02:09 -0500

    Added assessments table back into the ETL.

--- etl/app/server/profiles/index.js
+++ etl/app/server/profiles/index.js
@@ -1,6 +1,5 @@
-// TODO uncomment the assessments profile it becomes more stable
 export * as actions from './actions.js';
-// export * as assessments from './assessments.js';
+export * as assessments from './assessments.js';
 export * as assessmentUnits from './assessmentUnits.js';
 export * as assessmentUnitsMonitoringLocations from './assessmentUnitsMonitoringLocations.js';
 export * as catchmentCorrespondence from './catchmentCorrespondence.js';
