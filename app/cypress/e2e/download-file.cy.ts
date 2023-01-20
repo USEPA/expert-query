f710e7e447801d23777602b1f2d91490d69699dc
ergjustin <justin.joseph.erg.com>
2023-01-20 10:53:22 -0600

    download file length

--- app/cypress/e2e/download-file.cy.ts
+++ app/cypress/e2e/download-file.cy.ts
@@ -2,17 +2,18 @@ describe("Download file", () => {
   before(() => {
     cy.visit("/");
   });
-let count = "0"
+
+  let count = "0";
   it("Download and Verify Alert message visible when download and hidden after 10 second", () => {
-    cy.selectProfile("Assessments")
+    cy.selectProfile("Assessments");
     cy.get("#input-state").type("texas{downArrow}{enter}");
     cy.findByText("JavaScript Object Notation (JSON)").click();
     cy.findByRole("button", { name: "Download" }).click();
     cy.wait(2000);
     cy.findByText("Continue").should("exist");
-    cy.findByTestId('downloadfile-length').then(($el)=>{
-      count = $el.text()
-    })
+    cy.findByTestId("downloadfile-length").then(($el) => {
+      count = $el.text();
+    });
     cy.findByText("Continue").click();
     cy.findByText("Query executed successfully.").should("exist");
     cy.wait(10000);
@@ -24,6 +25,7 @@ let count = "0"
     cy.readFile(path, { timeout: 25000 }).should("exist");
     cy.readFile(path).should("be.a", "array");
     cy.readFile(path).should("have.length.at.least", 1);
+    cy.readFile(path).should("have.length", count);
     cy.readFile(path).then((item) => {
       const firstElement = item[0];
       const property = [
