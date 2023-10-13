76161862ea42bb50db301a246a3bf87a75f38c48
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2023-10-13 06:48:37 -0400

    EQ-340 In tsconfig, switched to verbatimModuleSyntax as importsNotUsedAsValues is deprecated.

--- app/client/src/components/errorBoundary.tsx
+++ app/client/src/components/errorBoundary.tsx
@@ -1,4 +1,5 @@
-import { Component, ErrorInfo, ReactNode } from 'react';
+import { Component } from 'react';
+import type { ErrorInfo, ReactNode } from 'react';
 // components
 import { Message } from 'components/message';
 
