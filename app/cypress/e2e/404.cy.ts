151bae513fe81f7fff4e3a9c81190a93d9814ea1
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2024-06-14 08:18:02 -0400

    Added a few more cypress tests.

---
+++ app/cypress/e2e/404.cy.ts
@@ -0,0 +1,7 @@
+describe('404 Page', () => {
+  it('404 Redirect', () => {
+    cy.visit('/bogus-route');
+    cy.url().should('eq', 'http://localhost:9090/404.html?src=http://localhost:3000/bogus-route');
+    cy.findByRole('heading', { name: 'Sorry, but this web page does not exist.' }).should('be.visible')
+  });
+});
